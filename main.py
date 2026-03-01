f809c9ca5052f0b882d974bbbb36f94a4b7a9d96
gibarol <gibarol@gmail.com>
2026-03-01 09:49:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
 
 @app.route("/health")
 def health():
     return jsonify({"status": "ok", "service": "api-presenca-teste"})
+
+@app.route("/ip")
+def ip():
+    response = requests.get("https://api.ipify.org?format=json")
+    return jsonify(response.json())
